7210f508273e4fbf587677e22625eb903c81134a
Yogesh Murdeshwar <yogeshpm89@gmail.com>
2025-01-28 18:37:26 +0530

    test

--- tasks/402517/index.test.js
+++ tasks/402517/index.test.js
@@ -1,4 +1,4 @@
-const { getMonthlyCalendar } = require('./correct');
+const { getMonthlyCalendar } = require(process.env.TARGET_FILE);
 
 describe('getMonthlyCalendar', () => {
     it('should generate a calendar for October 2023 starting on Sunday', () => {
